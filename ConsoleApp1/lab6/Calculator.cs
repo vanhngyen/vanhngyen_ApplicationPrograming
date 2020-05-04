8bdf22fe180f15137bb52db339ef42b8865546d7
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-05-04 16:01:46 +0700

    fix

--- ConsoleApp1/lab6/Calculator.cs
+++ ConsoleApp1/lab6/Calculator.cs
@@ -32,6 +32,17 @@ namespace ConsoleApp1.lab6
             Console.WriteLine(a +" * "+b+" = "+(a*b));
             return a * b;
         }
-        
+
+        public static Cclt getCalc(int choose)
+        {
+            switch (choose)
+            {
+                case 1: return Add;
+                case 2: return Sub;
+                case 3: return Mul;
+                case 4: return div;
+                default: return null;
+            }
+        }
     }
 }
