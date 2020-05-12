83b8ec4af714fe47405dc63a7909be4785b7fe5b
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-05-12 10:28:15 +0700

    exam

--- ConsoleApp1/practical/Exam2/animal.cs
+++ ConsoleApp1/practical/Exam2/animal.cs
@@ -2,6 +2,52 @@
 {
     public class animal
     {
+        public string name;
+        public int weight;
+
+        public animal()
+        {
+        }
+
+        public animal(string name, int weight)
+        {
+            this.name = name;
+            this.weight = weight;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Weight
+        {
+            get => weight;
+            set => weight = value;
+        }
         
+        public class tiger : animal
+        {
+            public tiger()
+            {
+            }
+
+            public tiger(string name, int weight) : base(name, weight)
+            {
+                
+            }
+        }
+        
+        public class lion : animal
+        {
+            public lion()
+            {
+            }
+
+            public lion(string name, int weight) : base(name, weight)
+            {
+            }
+        }
     }
 }
---
+++ ConsoleApp1/practical/Exam2/show.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1.practical.Exam2
+{
+    public class show
+    {
+        public static void Main(string[] args)
+        {
+            animal.tiger a = new animal.tiger("tiger",100);
+            animal.lion b = new animal.lion("lion",200);
+            Console.WriteLine("---- animal ----");
+            Console.WriteLine("Name = {0}", a.Name);
+            Console.WriteLine("Weight = {0}", a.Weight);
+            Console.WriteLine("Name = {0}", b.Name);
+            Console.WriteLine("Weight = {0}", b.Weight);
+            
+        }
+    }
+}
